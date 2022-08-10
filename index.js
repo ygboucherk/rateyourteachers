28db57bdb1e33ba834ba5af35f7d2640b6f3db5f
ygboucherk <ygboucherk@gmail.com>
2022-08-10 13:03:52 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-function connectWallet() {
-	if !(window.ethereum) {
-		console.error("NO METAMASK DETECTED");
-		return false;
+class Wallet {
+	constructor() {
+		this.connect();
 	}
-	return Web3(window.ethereum);
+	
+	async connect() {
+		if (window.ethereum) {
+			const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+			const account = accounts[0]
+			this.web3 = new Web3(window.ethereum);
+		}
+		else {
+			throw "NO METAMASK DETECTED";
+		}
+	}
 }
+
+wallet = new Wallet();
